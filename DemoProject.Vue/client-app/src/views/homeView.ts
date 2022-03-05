1837b443e4dc545e8610b2226416c127e84fadfe
shamonj03 <shamonj03@users.noreply.github.com>
2022-03-04 20:15:26 -0600

    Add project files.

---
+++ DemoProject.Vue/client-app/src/views/homeView.ts
@@ -0,0 +1,9 @@
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+	name: "HomeView",
+	components: {
+		HelloWorld,
+	},
+});
